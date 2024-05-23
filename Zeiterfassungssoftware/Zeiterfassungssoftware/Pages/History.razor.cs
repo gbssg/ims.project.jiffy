e306736b152fcb2c1ce135ed8c7e9d9de8346f3b
Runo3 <142808115+Runo3@users.noreply.github.com>
2024-05-23 08:38:49 +0200

    History.razor.cs

--- Zeiterfassungssoftware/Zeiterfassungssoftware/Pages/History.razor.cs
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Pages/History.razor.cs
@@ -1,9 +1,16 @@
+using Zeiterfassungssoftware.Data;
+
 namespace Zeiterfassungssoftware.Pages
 {
     public partial class History
     {
 
-        
+        public List<TimeEntry> Entries;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Entries = new TimeEntryProvider().LoadTimeEntries();
+        }
 
     }
 }
