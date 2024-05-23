65611171aed63cfb2f2ec2e233d7c08a0ec81d1b
Runo3 <142808115+Runo3@users.noreply.github.com>
2024-05-23 15:07:42 +0200

    Holiday Detection

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Data/Holidays/HolidayProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+
+namespace Zeiterfassungssoftware.Data.Holidays
+{
+    public class HolidayProvider
+    {
+        public async Task<Holidays> LoadHolidaysAsync() 
+        {
+            HttpClient http = new HttpClient();
+            Holidays Holidays = await http.GetFromJsonAsync<Holidays>("https://daten.stadt.sg.ch/api/explore/v2.1/catalog/datasets/schulferien-feiertage-stadt-stgallen/records?limit=100");
+
+            Holidays.results = Holidays.results.Where(e => e.betreff.ToLower().Contains("ferien")).Where(e => e.GetEndAsDate() >= new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)).ToList();
+
+            return Holidays;
+        }
+
+        public static async Task<bool> IsHolyDay() 
+        {
+            Holidays Holidays = await new HolidayProvider().LoadHolidaysAsync();
+
+            DateOnly Now = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+            foreach (Holiday Holiday in Holidays.results)
+            {
+                if ((Now > Holiday.GetBeginAsDate()) && (Holiday.GetEndAsDate() > Now))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Data/Holidays/Holidays.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Zeiterfassungssoftware.Data.Holidays
+{
+    public class Holidays
+    {
+        public int total_count { get; set; }
+        public List<Holiday> results { get; set; }
+
+        public static implicit operator Holidays?(Holiday? v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
