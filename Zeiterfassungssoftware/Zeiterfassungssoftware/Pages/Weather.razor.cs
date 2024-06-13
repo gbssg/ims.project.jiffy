f285d0a8e189cbe20a97f26d2ad3e339b12d2b47
Tenzin.T <tseringtenzin960@gmail.com>
2024-06-13 08:31:57 +0200

    MainLayout changes and Time Page structure

--- Zeiterfassungssoftware/Zeiterfassungssoftware/Pages/Weather.razor.cs
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Pages/Weather.razor.cs
@@ -2,12 +2,5 @@ using Microsoft.AspNetCore.Components;
 
 namespace Zeiterfassungssoftware.Pages
 {
-	public partial class Weather
-	{
-		public string message = string.Empty;	
-		public void OnClick(ChangeEventArgs args)
-		{
-			message = args.Value.ToString();
-		}
-	}
+
 }
