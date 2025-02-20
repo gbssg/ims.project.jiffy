176e096f1cffad43c60ada165efa80a8412dce45
Runo3 <142808115+Runo3@users.noreply.github.com>
2025-02-20 11:00:19 +0100

    Add initial migration for Jiffy DB

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware/Data/Jiffy/Migrations/20250220095751_InitialMigration.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zeiterfassungssoftware.Data.Jiffy.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Klasse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Start_Jahr = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Klasse", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityDescription",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityDescription", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActivityDescription_AspNetUsers",
+                        column: x => x.User_Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityTitle",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityTitle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActivityTitle_AspNetUsers",
+                        column: x => x.User_Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entry",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entry", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entry_AspNetUsers",
+                        column: x => x.User_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityDescription_User_Id",
+                table: "ActivityDescription",
+                column: "User_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityTitle_User_Id",
+                table: "ActivityTitle",
+                column: "User_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entry_User_id",
+                table: "Entry",
+                column: "User_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityDescription");
+
+            migrationBuilder.DropTable(
+                name: "ActivityTitle");
+
+            migrationBuilder.DropTable(
+                name: "Entry");
+
+            migrationBuilder.DropTable(
+                name: "Klasse");
+        }
+    }
+}
