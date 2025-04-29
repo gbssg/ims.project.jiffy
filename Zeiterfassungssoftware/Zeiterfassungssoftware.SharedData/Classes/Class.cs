32eaf5f3900c7ba545a7305fcefab74c42e83556
Runo3 <142808115+Runo3@users.noreply.github.com>
2025-04-29 13:56:15 +0200

    Created Class DTO

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware.SharedData/Classes/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeiterfassungssoftware.SharedData.Classes
+{
+    public class Class
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public TimeSpan[] ShouldTimes { get; set; } = new TimeSpan[5];
+    }
+}
---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware.SharedData/Classes/IClassProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeiterfassungssoftware.SharedData.Time;
+
+namespace Zeiterfassungssoftware.SharedData.Classes
+{
+    public interface IClassProvider
+    {
+        public bool IsLoaded { get; set; }
+        public void Add(Class Class);
+        public void Remove(Class Entry);
+        public Task<Class> GetClassById(Guid Id);
+        public List<Class> GetClasses();
+        public Task Update(Class Class);
+
+
+    }
+}
