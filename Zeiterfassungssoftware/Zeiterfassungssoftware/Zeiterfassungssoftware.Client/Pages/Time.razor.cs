52ad99f947227e6a8a3067be4ccab547bcd68b38
Runo3 <142808115+Runo3@users.noreply.github.com>
2025-12-04 13:59:56 +0100

    refactor(time): refactored time page

--- Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/Time.razor.cs
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/Time.razor.cs
@@ -17,25 +17,27 @@ namespace Zeiterfassungssoftware.Client.Pages
 		private IActivityProvider ActivitySource { get; init; }
 
 
-		public bool Started => CurrentEntry != null && CurrentEntry.End == null;
-		public bool Disabled => !(Started || !((ActivityTitleSelect.Equals(NEW_ACTIVITY_TITLE) && ActivityTitle.Trim().Equals("")) || (ActivityDescriptionSelect.Equals(NEW_ACTIVITY_DESCRIPTION) && ActivityDescription.Trim().Equals(""))));
-		private TimeSpan PassedTime => (DateTime.Now - CurrentEntry?.Start) ?? TimeSpan.Zero;
-
+		public bool Started => CurrentEntry is not null 
+							   && CurrentEntry.End is null;
+		public bool Disabled => !Started && (string.IsNullOrWhiteSpace(Title) || string.IsNullOrWhiteSpace(Description));
+		
+		
 		public TimeEntryDto? CurrentEntry { get; set; }
+		private TimeSpan PassedTime => (DateTime.Now - CurrentEntry?.Start) ?? TimeSpan.Zero;
 		private Timer? Timer;
 
 
 		private string ActivityTitleSelect = NEW_ACTIVITY_TITLE;
 		private string ActivityTitle = string.Empty;
-		private string Title => ActivityTitleSelect.Equals(NEW_ACTIVITY_TITLE) ? ActivityTitle : ActivityTitleSelect;
+		private string Title => string.Equals(ActivityTitleSelect, NEW_ACTIVITY_TITLE) ? ActivityTitle : ActivityTitleSelect;
 
 
 		private string ActivityDescriptionSelect = NEW_ACTIVITY_DESCRIPTION;
 		private string ActivityDescription = string.Empty;
-		private string Description => ActivityDescriptionSelect.Equals(NEW_ACTIVITY_DESCRIPTION) ? ActivityDescription : ActivityDescriptionSelect;
+		private string Description => string.Equals(ActivityDescriptionSelect, NEW_ACTIVITY_DESCRIPTION) ? ActivityDescription : ActivityDescriptionSelect;
 
-		public string ActivityDescriptionTextAreaStyle => ActivityDescriptionSelect.Equals(NEW_ACTIVITY_DESCRIPTION) ? "" : "display: none;";
-		public string ActivityTitleTextAreaStyle => ActivityTitleSelect.Equals(NEW_ACTIVITY_TITLE) ? "" : "display: none;";
+		public string ActivityDescriptionTextAreaStyle => string.Equals(ActivityDescriptionSelect, NEW_ACTIVITY_DESCRIPTION) ? "" : "display: none;";
+
 
 		public bool Loaded = false;
 
@@ -47,92 +49,81 @@ namespace Zeiterfassungssoftware.Client.Pages
 
 		public void Init()
 		{
-            CurrentEntry = TimeEntrySource.GetEntries().FirstOrDefault();
-
-            if ((CurrentEntry is not null) && (CurrentEntry.End is not null))
-                CurrentEntry = null;
+            CurrentEntry = TimeEntrySource.GetEntries().FirstOrDefault(e => e.End is null);
 
             if (!Started)
 				Timer?.Dispose();
 		}
 
-		private async void ToggleClock()
+		public async void ToggleClock()
 		{
 			if (Disabled)
 				return;
 
-			if (string.Equals(Title, "Krank"))
-            {
-				TimeEntryDto Entry = new()
-				{
-					Start = new DateTime(DateOnly.FromDateTime(DateTime.Now), new TimeOnly(0,0,0)),
-					End = new DateTime(DateOnly.FromDateTime(DateTime.Now), new TimeOnly(23, 59, 59)),
-					Title = "Krank",
-					Description = this.Description,
-				};
-
-				await TimeEntrySource.CreateEntry(Entry);
-				
-                ResetValues();
-                return;
-			}
-
 			if (!Started)
 			{
-				if (ActivityTitleSelect.Equals(NEW_ACTIVITY_TITLE))
-				{
-					var Title = new ActivityTitleDto()
+				PostTitle();
+				PostDescription();
+
+				Timer = new Timer(UpdateTimer, null, 0, 1000);
+
+				CurrentEntry = await TimeEntrySource.CreateEntry(
+					new()
 					{
-						Value = ActivityTitle,
-					};
-
-                    if (!ActivitySource.GetTitles().Any(e => e.Value == ActivityTitle))
-                        await ActivitySource.CreateTitle(Title);
-				}
-
-
-				if (ActivityDescriptionSelect.Equals(NEW_ACTIVITY_DESCRIPTION))
-				{
-                    var Description = new ActivityDescriptionDto()
-                    {
-                        Value = ActivityDescription,
-                    };
-
-                    if (!ActivitySource.GetDescriptions().Any(e => e.Value == ActivityDescription))
-						await ActivitySource.CreateDescription(Description);
-				}
-
-				Timer = new System.Threading.Timer(UpdateTimer, null, 0, 1000);
-				CurrentEntry = new()
-				{
-					Start = DateTime.Now,
-					Title = this.Title,
-					Description = this.Description,
-				};
-
-				CurrentEntry = await TimeEntrySource.CreateEntry(CurrentEntry);
+						Start = DateTime.Now,
+						Title = this.Title,
+						Description = this.Description,
+					}
+				);
             }
-			else if (CurrentEntry != null)
-			{
-				Timer?.Dispose();
-				CurrentEntry.End = DateTime.Now;
-
-
-				if (CurrentEntry.Time < new TimeSpan(0, 0, 1))
-					await TimeEntrySource.CreateEntry(CurrentEntry);
-				else
-					await TimeEntrySource.UpdateEntry(CurrentEntry.Id, CurrentEntry);
-
-				ResetValues();
-			}
 			else
-			{
+			{  
+				if (CurrentEntry is null)
+					return;
+
+				CurrentEntry.End = DateTime.Now;
+				await TimeEntrySource.UpdateEntry(CurrentEntry.Id, CurrentEntry);
+				
 				Timer?.Dispose();
-				throw new Exception("CurrentEntry was null even though the measuring was not started!");
+				ResetValues();
 			}
 		}
 
-		public void ResetValues()
+		public async void PostTitle()
+		{
+            if (string.Equals(ActivityTitleSelect, NEW_ACTIVITY_TITLE))
+            {
+				if (string.Equals(ActivityTitle, NEW_ACTIVITY_TITLE))
+					return;
+
+                var Title = new ActivityTitleDto()
+                {
+                    Value = ActivityTitle,
+                };
+
+                if (!ActivitySource.GetTitles().Any(e => e.Value == ActivityTitle))
+                    await ActivitySource.CreateTitle(Title);
+            }
+        }
+
+        public async void PostDescription()
+        {
+            if (string.Equals(ActivityDescriptionSelect, NEW_ACTIVITY_DESCRIPTION))
+            {
+                if (string.Equals(ActivityDescription, NEW_ACTIVITY_DESCRIPTION))
+                    return;
+
+                var Description = new ActivityDescriptionDto()
+                {
+                    Value = ActivityDescription,
+                };
+
+                if (!ActivitySource.GetDescriptions().Any(e => e.Value == ActivityDescription))
+                    await ActivitySource.CreateDescription(Description);
+            }
+        }
+
+        public void ResetValues()
 		{
             ActivityTitleSelect = NEW_ACTIVITY_TITLE;
             ActivityDescriptionSelect = NEW_ACTIVITY_DESCRIPTION;
