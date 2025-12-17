737cc9dc96feab0b95ecf67f2201a54dba6f41cd
Runo3 <142808115+Runo3@users.noreply.github.com>
2025-12-17 15:04:53 +0100

    refactor(history): started refactoring history #75

--- Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/History.razor.cs
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/History.razor.cs
@@ -11,11 +11,10 @@ namespace Zeiterfassungssoftware.Client.Pages
         public ITimeEntryProvider TimeEntrySource { get; set; }
 
         public Timer? Timer { get; set; }
-
-        public TimeSpan Overtime { get; set; } = new TimeSpan(0, 0, 0);
-        
-        public bool ShowFilters { get; set; }
         public int LastCount { get; set; }
+
+        public bool ShowFilters { get; set; }
+        public TimeSpan Overtime { get; set; } = new TimeSpan(0, 0, 0);
 
 
         private IFilter[] Filters =
@@ -27,7 +26,18 @@ namespace Zeiterfassungssoftware.Client.Pages
             new StringFilter("Description"),
             new StringFilter("Username"),
         ];
+
         private IFilter SelectedFilter => Filters.FirstOrDefault(e => e.PopUp);
+
+        private IEnumerable<TimeEntryDto> FilteredEntries =>
+            TimeEntrySource.GetEntries()
+                .Where(DoFiltersApply)
+                .Take(LastCount);
+
+        private string OvertimeDisplay =>
+            Math.Abs(Overtime.TotalHours) < 1
+                ? $"{Math.Floor(Overtime.TotalMinutes)}min"
+                : $"{Math.Floor(Overtime.TotalHours)}h";
 
         protected override void OnInitialized()
         {
@@ -38,17 +48,15 @@ namespace Zeiterfassungssoftware.Client.Pages
         protected void CalculateStats()
         {
             var Days = TimeEntrySource.GetEntries()
-                                       .GroupBy(e => e.Start.Date);
+                .GroupBy(e => e.Start.Date);
 
-            
             foreach(var Day in Days)
             {
                 var CurrentDay = Day.FirstOrDefault();
 
                 foreach (var Entry in Day)
-                {
                     Overtime += Entry.Time;
-                }
+                
                 Overtime -= CurrentDay.ShouldTime;
             }
         }
@@ -70,18 +78,21 @@ namespace Zeiterfassungssoftware.Client.Pages
                 Filter.PopUp = true;
         }
 
+
+        public bool DoFiltersApply(TimeEntryDto Entry)
+		{
+			return Filters[0].MatchesCriteria(Entry.Start) && 
+                   Filters[1].MatchesCriteria(Entry.Start) &&
+				   Filters[2].MatchesCriteria(Entry.End ?? DateTime.Now) && 
+                   Filters[3].MatchesCriteria(Entry.Title) &&
+				   Filters[4].MatchesCriteria(Entry.Description) && 
+                   Filters[5].MatchesCriteria(Entry.Username);
+		}
+
         void IDisposable.Dispose()
         { 
             Timer?.Dispose();
         }
-
-        public bool DoFiltersApply(TimeEntryDto Entry)
-		{
-			return Filters[0].MatchesCriteria(Entry.Start) && Filters[1].MatchesCriteria(Entry.Start) &&
-				        Filters[2].MatchesCriteria(Entry.End ?? DateTime.Now) && Filters[3].MatchesCriteria(Entry.Title) &&
-				        Filters[4].MatchesCriteria(Entry.Description) && (Filters[5].MatchesCriteria(Entry.Username));
-		}
-
         
     }
 }
