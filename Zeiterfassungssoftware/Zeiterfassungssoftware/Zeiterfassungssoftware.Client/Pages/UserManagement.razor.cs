5462f4195cac9eac24fb5d5f07b808d4c0a0cfce
Runo3 <142808115+Runo3@users.noreply.github.com>
2025-12-19 11:55:57 +0100

    feat(usermanagement): started creating a new page for managing users #79

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/UserManagement.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Zeiterfassungssoftware.SharedData.Users;
+
+namespace Zeiterfassungssoftware.Client.Pages
+{
+    public partial class UserManagement : ComponentBase
+    {
+
+        [Inject]
+        public IUserProvider UserProvider { get; set; }
+
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+
+        private Timer? Timer;
+
+        protected override void OnInitialized()
+        {
+            Timer = new Timer(UpdateTimer, null, 0, 1000);
+        }
+
+
+        public void UpdateTimer(object? obj)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnUserClicked(UserDto user)
+        {
+            Navigation.NavigateTo($"user/{user.Id}");
+        }
+    
+    }
+}
