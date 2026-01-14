f6c8fa5373d55d0c1b8dbfa147646e0750d0d9b8
Runo3 <142808115+Runo3@users.noreply.github.com>
2026-01-14 16:03:27 +0100

    feat(role): added a role dto and a mapper for it #83

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware/Mapper/RoleMapper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Zeiterfassungssoftware.SharedData.Roles;
+
+namespace Zeiterfassungssoftware.Mapper
+{
+    public class RoleMapper
+    {
+        public static RoleDto ToDto(IdentityRole identityRole)
+        {
+            if (identityRole is null)
+                throw new ArgumentNullException();
+
+            return new()
+            {
+                Id = identityRole.Id,
+                Name = identityRole.Name ?? "",
+                NormalizedName = identityRole.NormalizedName ?? "",
+                ConcurrencyStamp = identityRole.ConcurrencyStamp ?? ""
+            };
+        }
+
+        public static IdentityRole FromDto(RoleDto roleDto)
+        {
+            if (roleDto is null)
+                throw new ArgumentNullException();
+
+            return new()
+            {
+                Id = roleDto.Id,
+                Name = roleDto.Name,
+                NormalizedName = roleDto.NormalizedName,
+                ConcurrencyStamp = roleDto.ConcurrencyStamp
+            };
+        }
+    }
+}
