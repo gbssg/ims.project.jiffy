b276e571e4db5497226053cfc897fb8e36ca61d4
Runo3 <142808115+Runo3@users.noreply.github.com>
2026-01-14 13:36:52 +0100

    feat(edit): added the confirm deletion modal to edit entry #82

--- Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/Edit.razor.cs
+++ Zeiterfassungssoftware/Zeiterfassungssoftware/Zeiterfassungssoftware.Client/Pages/Edit.razor.cs
@@ -25,6 +25,7 @@ namespace Zeiterfassungssoftware.Client.Pages
 
         public Timer? Timer { get; set; }
 
+        public bool DisplayDeleteModal { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -55,6 +56,12 @@ namespace Zeiterfassungssoftware.Client.Pages
             UpdateEntry();
             await TimeEntrySource.UpdateEntry(Entry.Id, Entry);
             Navigation.NavigateTo("/history");
+        }
+
+        private void DeleteClicked()
+        {
+            DisplayDeleteModal = true;
+            InvokeAsync(StateHasChanged);
         }
 
         private async void DeleteEntry()
