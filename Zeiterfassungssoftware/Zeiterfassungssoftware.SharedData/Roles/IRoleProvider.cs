cfb8a27319b912eec0df07a3efaa67cf006deb52
Runo3 <142808115+Runo3@users.noreply.github.com>
2026-01-14 16:15:18 +0100

    feat(iroleprovider): added interface for roleprovider #83

---
+++ Zeiterfassungssoftware/Zeiterfassungssoftware.SharedData/Roles/IRoleProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeiterfassungssoftware.SharedData.Classes;
+
+namespace Zeiterfassungssoftware.SharedData.Roles
+{
+    public interface IRoleProvider
+    {
+        public bool IsLoaded { get; set; }
+
+        public Task<RoleDto> CreateRole(RoleDto role);
+        public Task<RoleDto> UpdateRole(Guid id, RoleDto role);
+        public Task DeleteRole(Guid id);
+        public Task<RoleDto> GetRoleById(Guid id);
+        public List<RoleDto> GetRoles();
+
+    }
+}
